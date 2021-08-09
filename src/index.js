5781e4f5f42c83f69cbcd157d9f34a44e77bf0c3
EugeneTerry <eugene.terry73@gmail.com>
2021-08-09 18:38:11 -0500

    added routing codes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { link } from 'react-router-dom';
+
 // import reportWebVitals from './reportWebVitals';
 import {Kennel} from "./components/Kennel"
 
