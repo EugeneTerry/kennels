68f2f7256f866e092b6be0b24e461d8dfeaa4f8a
EugeneTerry <eugene.terry73@gmail.com>
2021-08-16 18:32:20 -0500

    added a delete button that works

--- src/components/animal/AnimalDetails.js
+++ src/components/animal/AnimalDetails.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AnimalContext } from "./AnimalProvider";
 import "./Animal.css";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 export const AnimalDetail = () => {
-  const { animals } = useContext(AnimalContext);
+  const { animals, releaseAnimal } = useContext(AnimalContext)
+  // const { animals } = useContext(AnimalContext);
   const [animal, setAnimal] = useState({ location: {}, customer: {} });
 
   /*
@@ -12,6 +13,18 @@ export const AnimalDetail = () => {
         of 5 in the animalId variable
     */
   const { animalId } = useParams();
+  // Update this line of code to include releaseAnimal
+  
+
+  const history = useHistory()
+
+  const handleRelease = () => {
+    releaseAnimal(animal.id)
+      .then(() => {
+        history.push("/animals")
+      })
+  }
+
 
   useEffect(() => {
     const thisAnimal = animals.find((a) => a.id === parseInt(animalId)) || {
@@ -23,11 +36,16 @@ export const AnimalDetail = () => {
   }, [animalId]);
 
   return (
+    <>
+    <button onClick={handleRelease}>
+      Release Animal
+    </button>
     <section className="animal">
       <h3 className="animal__name">{animal.name}</h3>
       <div className="animal__breed">{animal.breed}</div>
       <div className="animal__location">Location: {animal.location.name}</div>
       <div className="animal__owner">Customer: {animal.customer.name}</div>
     </section>
+  </>
   );
 };
